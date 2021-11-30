1bdf5612c0ca3a23f0a4deaac24d10d07acaef7b
Christopher Rodriguez <yewscion@gmail.com>
2021-11-30 17:26:23 -0500

    Fixed formatting of `home-configuration.scm`.

--- home-configuration.scm
+++ home-configuration.scm
@@ -1,8 +1,9 @@
-;; This "home-environment" file can be passed to 'guix home reconfigure'
-;; to reproduce the content of your profile.  This is "symbolic": it only
-;; specifies package names.  To reproduce the exact same profile, you also
-;; need to capture the channels being used, as returned by "guix describe".
-;; See the "Replicating Guix" section in the manual.
+;; This "home-environment" file can be passed to 'guix home
+;; reconfigure' to reproduce the content of your profile.  This is
+;; "symbolic": it only specifies package names.  To reproduce the
+;; exact same profile, you also need to capture the channels being
+;; used, as returned by "guix describe".  See the "Replicating Guix"
+;; section in the manual.
 
 (use-modules
   (gnu home)
@@ -13,28 +14,6 @@
   (gnu packages bash))
 
 (home-environment
-  (channels (list (channel
-        (name 'yewscion)
-        (url "https://git.sr.ht/~yewscion/yewscion-guix-channel")
-        (branch "trunk")
-        (commit
-          "c488b1caf99c3a7beb0be79c4fa3242e955bf910")
-        (introduction
-          (make-channel-introduction
-            "3274a13809e8bad53e550970d684035519818ea0"
-            (openpgp-fingerprint
-              "6E3D E92C 3D0A 0A4D 1CDD  33EC 8EF2 971E D0D0 35B8"))))
-      (channel
-        (name 'guix)
-        (url "https://git.savannah.gnu.org/git/guix.git")
-        (branch "master")
-        (commit
-          "653bcab96d70d25e98753681d48657fe6634f8c5")
-        (introduction
-          (make-channel-introduction
-            "9edb3f66fd807b096b48283debdcddccfea34bad"
-            (openpgp-fingerprint
-              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))))
   (packages
     (map (compose list specification->package+output)
          (list "adb"
@@ -158,8 +137,6 @@
                   ("..." . "cd ../..")
                   ("...." . "cd ../../..")
                   ("....." . "cd ../../../..")
-;                  ("alert" .
-;                   "notify-send --urgency=low -i \\\"$([ $? = 0 ] && echo terminal || echo error)\\\" \\\"$(history|tail -n1|sed -e '\\''s/^\\s*[0-9]\\+\\s*//;s/[;&|]\\s*alert$//'\\'')\\\"")
                   ("egrep" . "egrep --color=auto")
                   ("fgrep" . "fgrep --color=auto")
                   ("grep" . "grep --color=auto")
@@ -171,10 +148,31 @@
                   ("port" . "cd $(cat ~/.sd)")))
               (bash-profile
                (list (plain-file "bash-profile"
-                           "")))
+                                 (format #f "~a~%~a~%~a~a~a~%~a"
+                                         "function dir-git-branch()"
+                                         "{"
+                                         "git branch 2>/dev/null | "
+                                         "grep \"^*\" | "
+                                         "sed 's/* /</;s/$/> /'"
+                                         "}"))))
               (bashrc
                (list (plain-file "bashrc"
-                           "shopt -s checkwinsize autocd cdspell checkhash cmdhist direxpand extglob gnu_errfmt histappend lithist no_empty_cmd_completion progcomp sourcepath xpg_echo")))
+                                 (format #f "~@{~a~^ ~}"
+                                         "shopt -s"
+                                         "checkwinsize"
+                                         "autocd"
+                                         "cdspell"
+                                         "checkhash"
+                                         "cmdhist"
+                                         "direxpand"
+                                         "extglob"
+                                         "gnu_errfmt"
+                                         "histappend"
+                                         "lithist"
+                                         "no_empty_cmd_completion"
+                                         "progcomp"
+                                         "sourcepath"
+                                         "xpg_echo"))))
               (bash-logout
                 (list (plain-file
                         "bash_logout"
@@ -183,6 +181,17 @@
                '(("SHELL" . "bash")
                  ("HISTCONTROL" . "ignoreboth")
                  ("HISTTIMEFORMAT" . "true")
-                 ("PS1" . "\"\\[\\e[0;2;35m\\][\\[\\e[0;2;35m\\]\\#\\[\\e[0;2;35m\\].\\[\\e[0;2;35m\\]$?\\[\\e[0;2;35m\\]] \\[\\e[0;2;37m\\]{\\[\\e[0;2;37m\\]\\A\\[\\e[0;2;37m\\]} \\[\\e[0;2;90m\\]<\\[\\e[0;2;90m\\]$(git branch 2>/dev/null | grep '\"'\"'^*'\"'\"' | colrm 1 2)\\[\\e[0;2;90m\\]> \\[\\e[0;92m\\]\\u\\[\\e[0m\\]@\\[\\e[0;36m\\]\\h\\[\\e[0m\\]:\\[\\e[0;94m\\]\\W\\[\\e[0;94m\\]/\\[\\e[0m\\]\\$ \\[\\e[0m\\]\"")))
+                 ("PS1" .
+                  (string-append "\"\\[\\e[0;2;35m\\][\\[\\e[0;2;"
+                                 "35m\\]\\#\\[\\e[0;2;35m\\].\\[\\e["
+                                 "0;2;35m\\]0\\[\\e[0;2;35m\\]] "
+                                 "\\[\\e[0;2;37m\\]{\\[\\e[0;2;"
+                                 "37m\\]\\A\\[\\e[0;2;37m\\]} \\[\\e"
+                                 "[0;2;90m\\]\\$("
+                                 "dir-git-branch)"
+                                 "\\[\\e[0;92m\\]\\u\\[\\e[0m\\]@"
+                                 "\\[\\e[0;36m\\]\\h\\[\\e[0m\\]:"
+                                 "\\[\\e[0;94m\\]\\W\\[\\e[0;94m\\]"
+                                 "/\\[\\e[0m\\]$ \\[\\e[0m\\]\""))))
               (guix-defaults? #t)
               (package bash))))))
