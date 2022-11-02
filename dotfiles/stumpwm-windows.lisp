0a75c7a5f23457dae8374f4708e1097aa31748e1
Christopher Rodriguez <yewscion@gmail.com>
2022-11-01 20:59:06 -0400

    Cleaned up window rules, and fixed icecat.

--- dotfiles/stumpwm-windows.lisp
+++ dotfiles/stumpwm-windows.lisp
@@ -1,14 +1,10 @@
 (("Emacs" 0 T T :CLASS "Emacs" :INSTANCE "emacs")
- ("Default" 1 T T :CLASS "GNU IceCat")
+ ("Default" 1 T T :CLASS "icecat-default" :INSTANCE "Navigator" :ROLE "browser")
  ("Default" 0 T T :CLASS "URxvt")
  ("Messaging" 0 T ("y" T) :CLASS "Slack" :INSTANCE "slack" :TITLE NIL :ROLE
   "browser-window")
  ("Messaging" 1 T ("y" T) :CLASS "Google-chrome" :INSTANCE "google-chrome"
               :TITLE NIL :ROLE "browser")
- ("Messaging" 1 T ("y" T) :CLASS "Chromium" :INSTANCE "chromium"
+ ("Messaging" 1 T ("y" T) :CLASS "Chromium-browser" :INSTANCE "chromium-browser"
               :TITLE NIL :ROLE "browser")
- ("Messaging" 1 T ("y" T) :CLASS "chromiu;m-browser" :INSTANCE "chromium-browser"
-              :TITLE NIL :ROLE "browser")
- ("Messaging" 1 T ("y" T) :CLASS "Midori" :INSTANCE "midori"
-              :TITLE NIL)
  ("Messaging" 2 T ("y" T) :CLASS "discord"))
