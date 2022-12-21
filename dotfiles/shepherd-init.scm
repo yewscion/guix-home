a51dc66975882a35a5514c8698579124f511c4ff
Christopher Rodriguez <yewscion@gmail.com>
2022-12-20 21:06:44 -0500

    dotfiles/shepherd-services.scm: Added fcitx5 service.

--- dotfiles/shepherd-init.scm
+++ dotfiles/shepherd-init.scm
@@ -1,4 +1,5 @@
 (load "services.scm")
 (register-services emacs)
+(register-services fcitx5)
 (action 'shepherd 'daemonize) ; send shepherd into background
 (for-each start (list emacs)) ; services to start automatically
--- dotfiles/shepherd-services.scm
+++ dotfiles/shepherd-services.scm
@@ -1,8 +1,15 @@
 (define emacs
  (make <service>
    #:provides '(emacs)
-   #:requires '()
+   #:requires '(fcitx5)
    #:start (make-system-constructor "emacs --daemon")
    #:stop (make-system-destructor
            "emacsclient --eval \"(kill-emacs)\"")
    #:declarative? #f))
+
+(define fcitx5
+  (make <service>
+    #:provides '(fcitx5)
+    #:requires '()
+    #:start (make-system-constructor "fcitx5 -d")
+    #:stop (make-kill-destructor)))
