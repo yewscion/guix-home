91312e7e6b37e7f6e8fa9ee545d24bf5a3f82797
Christopher Rodriguez <yewscion@gmail.com>
2023-02-02 18:28:56 -0500

    armored-core.scm: New file.

---
+++ armored-core.scm
@@ -0,0 +1,8 @@
+;;; This list of packages is meant to be a list of guix packages I would need
+;;; to switch full-time to an ARM-based computer.
+
+(define armored-core
+  (list
+   "vlang"
+   "guile"
+   "emacs"))
