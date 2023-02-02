d2110e548fbda0374fae900888740a5d999480ec
Christopher Rodriguez <yewscion@gmail.com>
2023-02-02 17:27:23 -0500

    dotfiles/shepherd-init.scm: Merge shepherd-services.scm into file.

--- dotfiles/shepherd-init.scm
+++ dotfiles/shepherd-init.scm
@@ -1,5 +1,17 @@
-(load "services.scm")
-(register-services emacs)
-(register-services fcitx5)
+(define emacs
+ (make <service>
+   #:provides '(emacs)
+   #:requires '(fcitx5)
+   #:start (make-system-constructor "emacs --daemon")
+   #:stop (make-system-destructor
+           "emacsclient --eval \"(kill-emacs)\"")))
+
+(define fcitx5
+  (make <service>
+    #:provides '(fcitx5)
+    #:requires '()
+    #:start (make-system-constructor "fcitx5 -d")
+    #:stop (make-kill-destructor)))
+
+(register-services emacs fcitx5)
 (action 'shepherd 'daemonize) ; send shepherd into background
-(for-each start (list emacs)) ; services to start automatically
