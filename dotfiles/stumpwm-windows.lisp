26f8eb742d3ea8f1c22c813564e2d51e4335ea52
Christopher Rodriguez <yewscion@gmail.com>
2023-02-28 13:44:54 -0500

    dotfiles/stumpwm-windows.lisp: Make chrome & chromium less specific.

--- dotfiles/stumpwm-windows.lisp
+++ dotfiles/stumpwm-windows.lisp
@@ -3,8 +3,6 @@
  ("Default" 0 T T :CLASS "URxvt")
  ("Messaging" 0 T ("y" T) :CLASS "Slack" :INSTANCE "slack" :TITLE NIL :ROLE
   "browser-window")
- ("Messaging" 1 T ("y" T) :CLASS "Google-chrome" :INSTANCE "google-chrome"
-              :TITLE NIL :ROLE "browser")
- ("Messaging" 1 T ("y" T) :CLASS "Chromium-browser" :INSTANCE "chromium-browser"
-              :TITLE NIL :ROLE "browser")
+ ("Messaging" 1 T ("y" T) :CLASS "Google-chrome")
+ ("Messaging" 1 T ("y" T) :CLASS "Chromium-browser")
  ("Messaging" 2 T ("y" T) :CLASS "discord"))
